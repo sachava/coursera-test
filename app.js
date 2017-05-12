b4f736d4eaedcbd48a61b960b61232197f7ee957
Serhii <sachava.serg@gmail.com>
2017-05-12 16:35:43 +0300

    countries

--- app.js
+++ app.js
@@ -4,6 +4,8 @@
   .controller('MyFirstController', function ($scope) {
     $scope.name ="Serhii";
     $scope.val1 = "One";
+    $scope.cost = .5;
+    console.log($scope);
     $scope.sayHello = function () {
       return "Hello";
     }
