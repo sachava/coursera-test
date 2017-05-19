358475a56d88dfe97ca0f654fafcdcd2b59aea25
Serhii <sachava.serg@gmail.com>
2017-05-19 16:26:32 +0300

    Module3 new4

--- module3-solution/js/app.js
+++ module3-solution/js/app.js
@@ -4,13 +4,21 @@
 	 angular.module('NarrowItDownApp', [])
 			.controller('NarrowItDownController', NarrowItDownController)
 			.service('MenuSearchService', MenuSearchService)
-			// // .directive()
+			.directive('foundItems', foundItemsDirective)
 			.constant('URL', 'http://davids-restaurant.herokuapp.com/menu_items.json');
+
+	
+	function foundItemsDirective () {
+		var ddo = {
+			templateUrl: "../founditems.html"
+		}
+		return ddo;
+	};
 
 	NarrowItDownController.$inject = ['MenuSearchService'];
 	function NarrowItDownController(MenuSearchService) {
 	   var NarrowItDownController = this;
-	   NarrowItDownController.items =[];
+	   NarrowItDownController.items = [];
 	   NarrowItDownController.warning = "";
 	   NarrowItDownController.searchstring = "";
 
@@ -18,52 +26,42 @@
 	   	    if (NarrowItDownController.searchstring.trim()==="") {
 	   	    	NarrowItDownController.warning = "Nothing found! Enter correct value.";
 	   	    	NarrowItDownController.searchstring = "";
-	   	    	return;
-	   	    }
-	   		var promise = MenuSearchService.getmatchedMenuItems();
-	   		promise.then(
-						function success(response) {
- 							var resp_items = response.data.menu_items;
- 							// searching through items
- 							var found = [];
- 							var etalon = NarrowItDownController.searchstring;
- 							etalon = etalon.trim().toLowerCase();
- 							
- 							for (var i=0; i<resp_items.length; i++) {
- 								var desc = resp_items[i].description.toLowerCase();
- 								
- 								if (desc.indexOf(etalon) !== -1) {
- 										found.push(resp_items[i]);
- 								}
- 								
- 							};
-
- 							NarrowItDownController.items = found;
-
- 							if (found.length == 0) {
- 								NarrowItDownController.warning = "Nothing found!"; 								
- 							} 
-						})
-	   					.catch(
-							function errors (response) {
-								NarrowItDownController.items = [];
-								NarrowItDownController.warning = "It happens something wrong!";
-								console.log(response)
-							});
-
-	  };
+	   	    } else {
+	   		    NarrowItDownController.items = MenuSearchService.getmatchedMenuItems(NarrowItDownController.searchstring);
+	    	};
+	    };
 	};
 
 	MenuSearchService.$inject = ['URL', '$http'];
 	function MenuSearchService (URL, $http) {
 		var MenuSearchService = this;
-
-		MenuSearchService.getmatchedMenuItems = function (searchTerm) {
-			var response = $http({
-							method: "GET",
-							url: URL
+		var items = [];
+		
+		MenuSearchService.getmatchedMenuItems = function (searchName) {
+			items = [];
+			searchName = searchName.trim().toLowerCase();
+			$http({ method: "GET",
+					url: URL
+			}).then(
+					 function success(response) {
+ 						var resp_items = response.data.menu_items; 							
+ 							for (var i=0; i<resp_items.length; i++) {
+ 								if (resp_items[i].description.toLowerCase().indexOf(searchName) !== -1) {
+ 										items.push(resp_items[i]);
+ 								}
+ 								
+ 							};
+ 							if (items.length == 0) {
+ 								MenuSearchService.warning = "Nothing found!"; 								
+ 							} 
+						})
+	   		.catch(
+					function errors (response) {
+							  items = [];
+							  MenuSearchService.warning = "It happens something wrong!";
+							  console.log(response)
 							});
-			return response;
+	   		return items;
 		}
 
 	}
